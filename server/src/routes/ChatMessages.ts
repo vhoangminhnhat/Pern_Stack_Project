b182deb1013bf44b5d0d7b7e3cfa4a1e8ee9be8a
Vu Hoang Minh Nhat <90960555+vhoangminhnhat@users.noreply.github.com>
2024-10-31 11:04:04 +0700

    sub routes logic adjustment

--- server/src/routes/ChatMessages.ts
+++ server/src/routes/ChatMessages.ts
@@ -4,14 +4,13 @@ import {
   sendMessages,
 } from "../controllers/chatMessages/index.js";
 import protectedRoutes from "../middlewares/protectedRoutes.js";
-import { getAppApiBase } from "../utils/helpers.js";
 
 const chatRouter = express.Router();
 
-chatRouter.get(getAppApiBase("/messages/:id"), chatMessages as any);
+chatRouter.get("/messages/:id", chatMessages as any);
 
 chatRouter.post(
-  getAppApiBase("/messages/send/:id"),
+  "/messages/send/:id",
   protectedRoutes as any,
   sendMessages as any
 );
