0ea04c5e8e45ca8ea13fc69333b6e27f64aa0b82
Vu Hoang Minh Nhat <90960555+vhoangminhnhat@users.noreply.github.com>
2024-11-01 13:53:22 +0700

    Chore: tailwindcss configuration for client

---
+++ client/tailwind.config.js
@@ -0,0 +1,13 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    "./src/**/*.ts",
+    "./src/**/*.js",
+    "./src/**/*.jsx",
+    "./src/**/*.tsx",
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
