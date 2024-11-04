b1d45c6c28661507b296e5032d054711af645cac
Vu Hoang Minh Nhat <90960555+vhoangminhnhat@users.noreply.github.com>
2024-11-04 13:20:27 +0700

    app config for main index.ts

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -1,10 +1,13 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from 'App'
+import App from "App";
+import { ContextProvider } from "context/AuthenticationContext";
+import { createRoot } from "react-dom/client";
+import { BrowserRouter as Router } from "react-router-dom";
+import "./index.css";
 
-createRoot(document.getElementById('root')!).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
-)
+createRoot(document.getElementById("root")!).render(
+  <Router>
+    <ContextProvider>
+      <App />
+    </ContextProvider>
+  </Router>
+);
